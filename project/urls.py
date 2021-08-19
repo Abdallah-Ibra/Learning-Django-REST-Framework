28922c855bdaa5a6992e34e03299385ccfa9fc0c
Abdallah Ibra <75906593+Abdallah-Ibra@users.noreply.github.com>
2021-08-19 22:47:06 +0300

    working with serializers.Serializer

--- project/urls.py
+++ project/urls.py
@@ -13,28 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-# from django.urls import path, include
+from django.contrib import admin
+from django.urls import path, include
 
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     ### Default Authorization URL
-#     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-#     ]
-
-
-
-from django.urls import include, path
-from rest_framework import routers
-from my_api import views
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
+    path('admin/', admin.site.urls),
+    ### Default Authorization URL
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    ]
+
+
