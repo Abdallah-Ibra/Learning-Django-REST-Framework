33bbf494280af9f8e837312c3795b4f6849797df
Abdallah Ibra <75906593+Abdallah-Ibra@users.noreply.github.com>
2021-08-21 01:30:17 +0300

    REST Class Based API Views

--- my_api/urls.py
+++ my_api/urls.py
@@ -3,7 +3,14 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+   
+   ### Function API View ###
+   path('api/v1/article/', views.article_list),
+   path('api/v1/article/<int:pk>', views.article_detail),
+   
+   ### Class Based API View ###
+   path('api/v2/article/', views.ArticleAPIView.as_view()),
+   path('api/v2/article/<int:id>', views.ArticleAPIViewDetails.as_view()),
+   
 
-   path('article/', views.article_list),
-   path('article/<int:pk>', views.article_detail),
    ]
